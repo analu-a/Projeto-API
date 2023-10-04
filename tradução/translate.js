25b66b8fa57e404f72f25eb0cb02a4e3a00e65bd
Ana Luiza <analuiza.azevedomorais@gmail.com>
2023-10-04 10:35:01 -0300

    translate minion funcionando

---
+++ tradução/translate.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const translateInput = document.getElementById('translateMinion')
+
+async function traduzir(translateMinion){
+    const url = `https://api.funtranslations.com/translate/minion.json?text=${translateMinion}`
+    const response = await fetch(url)
+    const minionTranslate = await response.json()
+    return minionTranslate
+}
+
+async function mostrarTraducao(){
+    const traduzido = document.getElementById('translateMinion')
+
+    const translateM = await traduzir (translateInput.value)
+    console.log (translateM)
+    traduzido.value = translateM.contents.translated
+}
+
+translateInput.addEventListener('focusout', mostrarTraducao)
+
